1210e76301ce6a5f685dfa38a30dc3f1b9f2fcae
Andrei Scafi <andreiscafi@hotmail.com>
2023-12-01 09:02:44 -0300

    Adding animation and focus function to search icon.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const searchIcon = document.querySelector('.search-icon');
 const searchInputWrapper = document.querySelector('.search-input-wrapper');
+const searchInput = document.querySelector('.search-input input');
 
 
 searchIcon.addEventListener('click', () => {
-    searchIcon.classList.toggle('change');
-    searchInputWrapper.classList.toggle('change');
+    searchIcon.classList.add('change');
+    searchInputWrapper.classList.add('change');
+
+    setTimeout(() => {
+        searchInput.focus()
+    }, 1000)
 })
